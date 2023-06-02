7e2aad3c6203a3ec39dd2f0ba8285c3601795089
rt <randytyng21@gmail.com>
2023-06-02 04:49:03 -0400

    Installed manim, followed tutorial, wrote up a scene
    Attempted a dot product scene after the initial tutorial. Did not turn out exactly how i wanted but can come back later

---
+++ project/dotproduct.py
@@ -0,0 +1,25 @@
+from manim import *
+
+#an attempt :D
+
+class DotProductScene(Scene):
+    def construct(self):
+        # Create vectors
+        vector1 = Vector([3, 1], color=RED)
+        vector2 = Vector([2, 2], color=BLUE)
+
+        # Add vectors to the scene
+        self.play(Create(vector1), Create(vector2))
+
+        # Calculate dot product
+        dot_product = np.dot(vector1.get_end(), vector2.get_end())
+
+        # Add dot product label
+        dot_product_label = MathTex(r"\text{Dot Product:}", r"3 \cdot 2 + 1 \cdot 2 =", str(dot_product))
+        dot_product_label.next_to(vector1.get_end(), direction=RIGHT)
+        self.play(Write(dot_product_label))
+
+        # Highlight the dot product calculation
+        self.play(ApplyMethod(dot_product_label[-1].set_color, YELLOW))
+
+        self.wait(2)
